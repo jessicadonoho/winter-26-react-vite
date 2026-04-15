1b385220f8d8531c2b88201c3c49fc980b6400c9
Jessica Donoho <jesiedu@jesiluv-3.local>
2026-04-15 10:23:58 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    base:'/winter-26-react-vite/',
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()], 
+  base:'/winter-26-react-vite/',
 })
